6736beb2327e2c263c1995130d9374f804b607fe
paragpujari <58661811+paragpujari@users.noreply.github.com>
2023-03-15 08:14:54 +0530

    Add files via upload

---
+++ Multtiplication.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def product(num1, num2):
+    print(num1 * num2)
+
+
+# In[ ]:
+
+
+
+
---
+++ Substraction.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def diff(num1, num2):
+    print(num1 - num2)
+
+
+# In[ ]:
+
+
+
+
